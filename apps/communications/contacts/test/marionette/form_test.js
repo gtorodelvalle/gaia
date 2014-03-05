a5195aa24ee51f852de4a3ea61a41537e6c4f62b
Germán Toro del Valle <gtorodelvalle@gmail.com>
2014-03-05 16:10:59 +0100

    Bug 972060 - [B2G][Dialer][Contacts] Imported Facebook contact does not show existing phone number when adding information via Dialer app

--- apps/communications/contacts/test/marionette/form_test.js
+++ apps/communications/contacts/test/marionette/form_test.js
@@ -1,21 +1,23 @@
 var Contacts = require('./lib/contacts');
+var Dialer = require('../../../dialer/test/marionette/lib/dialer');
 var assert = require('assert');
 
 marionette('Contacts > Form', function() {
-  var client = marionette.client(Contacts.config);
-  var subject;
-  var selectors;
+  var client = marionette.client(Contacts.config),
+    subject,
+    dialerSubject,
+    selectors;
 
   setup(function() {
     subject = new Contacts(client);
-    subject.launch();
-
     selectors = Contacts.Selectors;
+    dialerSubject = new Dialer(client);
+    dialerSelectors = Dialer.Selectors;
+    subject.launch();
   });
 
   suite('Click phone number', function() {
     test('Add a simple contact', function() {
-
       var givenName = 'Hello';
       var familyName = 'World';
 
@@ -83,4 +85,121 @@ marionette('Contacts > Form', function() {
     });
   });
 
+  suite('Facebook contacts', function() {
+    test('Add phone number from Dialer to existing Facebook contact',
+      function() {
+        var tel = '+34666666666',
+            email = 'gtorodelvalle@hotmail.com';
+
+        var saveFBContact = function(tel, email) {
+          var fb = window.wrappedJSObject.fb;
+
+          var fbContactData = {
+            'uid': '100007887283166',
+            'name': 'Jander Klander',
+            'pic_big': 'https://fbcdn-profile-a.akamaihd.net/static-ak/' +
+              'fbrsrc.php/v2/yL/r/HsTZSDw4avx.gif',
+            'current_location': null,
+            'email': [{
+              'type': ['other'],
+              'value': email
+        }],
+            'profile_update_time': 1393974966,
+            'work': [],
+            'hometown_location': null,
+            '_idxFriendsArray': 124,
+            'familyName': ['Klander'],
+            'additionalName': '',
+            'givenName': ['Jander'],
+            'tel': [{
+              'type': ['other'],
+              'value': tel
+          }],
+            'email1': email,
+            'contactPictureUri': 'https://graph.facebook.com/100007887283166/' +
+              'picture?type=square&width=120&height=120',
+            'search': 'JanderKlander' + email,
+            'picwidth': 120,
+            'picheight': 120,
+            '_idx_': 0,
+            'fbInfo': {
+              'org': [''],
+              'adr': [],
+              'shortTelephone': [tel.slice(3)],
+              'url': [{
+                'type': ['fb_profile_photo'],
+                'value': 'https://fbcdn-profile-a.akamaihd.net/static-ak/' +
+                  'rsrc.php/v2/yL/r/HsTZSDw4avx.gif'
+          }]
+            }
+          };
+
+          var fbContact = new fb.Contact();
+          fbContact.setData(fbContactData);
+
+          var savingFBContact = fbContact.save();
+
+          savingFBContact.onsuccess = function() {
+            marionetteScriptFinished(true);
+          };
+
+          savingFBContact.onerror = function() {
+            marionetteScriptFinished(false);
+          };
+        };
+
+        var isFBContactSaved = false;
+        client.executeAsyncScript(saveFBContact, [tel, email],
+          function(err, val) {
+          isFBContactSaved = val;
+        });
+
+        client.waitFor(function() {
+          return isFBContactSaved;
+        });
+
+        client.apps.close(Contacts.URL, 'contacts');
+
+        dialerSubject.launch();
+
+        var one = client.findElement(dialerSelectors.one),
+            two = client.findElement(dialerSelectors.two),
+            three = client.findElement(dialerSelectors.three);
+        for (var i = 0; i < 3; i++) {
+          one.tap();
+          two.tap();
+          three.tap();
+        }
+        var phoneNumber = dialerSubject.client.findElement(
+          dialerSelectors.phoneNumber);
+        client.waitFor(function() {
+          return (phoneNumber.getAttribute('value').length === 9);
+        });
+
+        var addContact = dialerSubject.client.findElement(
+          dialerSelectors.keypadCallBarAddContact);
+        addContact.tap();
+
+        var addToExistingContact = dialerSubject.client.helper.waitForElement(
+          dialerSelectors.addToExistintContactMenuItem);
+        addToExistingContact.tap();
+
+        client.switchToFrame();
+        client.apps.switchToApp(Contacts.URL, 'contacts');
+
+        client.findElement(selectors.listContactFirst).tap();
+
+        subject.waitForFormShown();
+
+        var formTelNumberSecond = client.helper.waitForElement(
+          selectors.formTelNumberSecond);
+        var formEmailFirst = client.helper.waitForElement(
+          selectors.formEmailFirst);
+
+        assert.equal(formTelNumberSecond.getAttribute('value'),
+               tel);
+        assert.equal(formEmailFirst.getAttribute('value'),
+               email);
+      });
+  });
 });
